df216c7f7dde757bd29cfdf2fa2f4de5fde0d7ba
prsanjay <sanjayp.live@gmail.com>
2025-12-19 18:08:28 +0000

    Refactor code and reset state on inactivity

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const inActivityThreshold = 3000;
+let timerId = null;
+
+
 let state = localStorage.getItem('state');
 
 if (state == null) {
@@ -9,62 +13,73 @@ if (state == null) {
 
 
 function increment() {
-    let currentValue = state["value"];
-    state["undoStack"].push(currentValue);
+    let currentValue = state.value;
+    state.undoStack.push(currentValue);
     currentValue += 1;
-    state["value"] = currentValue;
-    state["lastActionAt"] = Date.now();
-    localStorage.setItem("state", JSON.stringify(state));
-    renderValue();
+    state.value = currentValue;
+    state.redoStack = [];
+   commitState();
 }
 
 function decrement() {
-    let currentValue = state["value"];
-    state["undoStack"].push(currentValue);
+    let currentValue = state.value;
+    state.undoStack.push(currentValue);
     currentValue -= 1;
-    state["value"] = currentValue;
-    state["lastActionAt"] = Date.now();
-    localStorage.setItem("state", JSON.stringify(state));
-    renderValue();
+    state.redoStack = [];
+    state.value = currentValue;
+    commitState();
 }
 
-function manualReset() {
+function resetState() {
     state = {value: 0, undoStack: [], redoStack: [], lastActionAt: null}
     localStorage.setItem("state", JSON.stringify(state));
     renderValue();
 }
 
 function undo() {
-    let currentValue = state["value"];
-    state["redoStack"].push(currentValue);
-    let prevValue = state["undoStack"].pop();
-    state["value"] = prevValue == undefined ? currentValue : prevValue;
-    state["lastActionAt"] = Date.now();
-    localStorage.setItem("state", JSON.stringify(state));
-    renderValue();
+    let currentValue = state.value;
+    state.redoStack.push(currentValue);
+    let prevValue = state.undoStack.pop();
+    state.value = prevValue == undefined ? currentValue : prevValue;
+    commitState();
 }
 
 function redo() {
-    let currentValue = state["value"];
-    state["undoStack"].push(currentValue);
-    let val = state["redoStack"].pop();
-    state["value"] = val == undefined ? currentValue : val;
-    state["lastActionAt"] = Date.now();
+    let currentValue = state.value;
+    state.undoStack.push(currentValue);
+    let val = state.redoStack.pop();
+    state.value = val == undefined ? currentValue : val;
+    commitState();
+}
+
+function commitState() {
+    state.lastActionAt = Date.now();
     localStorage.setItem("state", JSON.stringify(state));
+    resetInactivityTimer();
     renderValue();
+}
+
+function resetInactivityTimer() {
+    clearTimeout(timerId);
+    timerId = setTimeout(resetState, inActivityThreshold);
 }
 
 
 document.getElementById("increment").addEventListener("click", increment);
 document.getElementById("decrement").addEventListener("click", decrement);
-document.getElementById("mr").addEventListener("click", manualReset);
+document.getElementById("mr").addEventListener("click", resetState);
 document.getElementById("undo").addEventListener("click", undo);
 document.getElementById("redo").addEventListener("click", redo);
 
 function renderValue() {
-    let currentValue = state["value"];
+    let currentValue = state.value;
     document.getElementById("value").innerText = currentValue;
     console.log("Current state:", state);
+}
+
+const lastActionAt = state.lastActionAt;
+if (lastActionAt !== null && (Date.now() - lastActionAt) > inActivityThreshold) {
+    resetState();
 }
 
 renderValue();
